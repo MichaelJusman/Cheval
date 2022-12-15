1c4dfed42009df354fb323c1f73e7f06d7fb0a52
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2022-12-16 01:57:43 +1100

    Boomerang 4

--- Assets/Script/Cromerang.cs
+++ Assets/Script/Cromerang.cs
@@ -10,32 +10,37 @@ public class Cromerang : GameBehaviour
 
     [Header("Boomerang Variables")]
     bool isBoomin;
-    Vector2 startPos;
+    GameObject startPos;
     GameObject player;
 
     private void Start()
     {
         Physics2D.IgnoreLayerCollision(3, 3);
         isBoomin = false;
+
+        player = GameObject.Find("Player");
+        startPos = GameObject.Find("Hand2");
+
+
         StartCoroutine(Boomerang());
 
     }
 
     private void Update()
     {
-        transform.Rotate(0, Time.deltaTime * 500, 0);
+        transform.Rotate(0, 0, Time.deltaTime * 200);
 
         if(isBoomin)
         {
-            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, Time.deltaTime * 40);
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, Time.deltaTime * 20);
         }
 
         if(!isBoomin)
         {
-            transform.position = Vector2.MoveTowards(transform.position, startPos, Time.deltaTime * 40);
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(startPos.transform.position.x, transform.position.y), Time.deltaTime * 20);
         }
 
-        if (!isBoomin && Vector2.Distance(startPos, transform.position) < 1.3f)
+        if (!isBoomin && Vector2.Distance(startPos.transform.position, transform.position) < 1.3f)
         {
             Die();
         }
@@ -44,7 +49,7 @@ public class Cromerang : GameBehaviour
     IEnumerator Boomerang()
     {
         isBoomin = true;
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(5f);
         isBoomin = false;
     }
 
